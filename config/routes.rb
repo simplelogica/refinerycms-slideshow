4b502378354c7883c4baca217402c1a570886fb0
David J. Brenes <david.brenes@simplelogica.net>
2013-07-31 13:30:19 +0200

    Reordering slides

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Refinery::Core::Engine.routes.append do
   namespace :slideshow, path: '' do
     namespace :admin, :path => 'refinery' do
       resources :slideshows, :except => :show do
-        resources :slides, :except => :show
+        resources :slides, :except => :show do
+          collection do
+            post :update_positions
+          end
+        end
       end
     end
   end
