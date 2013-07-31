9e6ee075a9d9e79037cdccde0f44e4272853f9aa
David J. Brenes <david.brenes@simplelogica.net>
2013-07-31 12:28:13 +0200

    Slides index in the slideshow edit page

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,7 @@ Refinery::Core::Engine.routes.append do
   namespace :slideshow, path: '' do
     namespace :admin, :path => 'refinery' do
       resources :slideshows, :except => :show do
-        collection do
-          post :update_positions
-        end
-        member do
-          post :merge
-        end
+        resources :slides, :except => :show
       end
     end
   end
