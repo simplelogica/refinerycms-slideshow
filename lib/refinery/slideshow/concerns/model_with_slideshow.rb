27f21e23d898ab937f517e31bc032a300d85b62c
David J. Brenes <david.brenes@simplelogica.net>
2013-09-02 16:52:31 +0200

    We create attached slideshows on-demand it object requires it
    Instead of creating a slideshow ALWAYS when an object is created we only create the attached slideshow if a certain method returns true
    
    We try to create it after save or every time an slideshow object is accessed

--- lib/refinery/slideshow/concerns/model_with_slideshow.rb
+++ lib/refinery/slideshow/concerns/model_with_slideshow.rb
@@ -15,6 +15,29 @@ module Refinery::Slideshow::Concerns::ModelWithSlideShow
       # We create the relationship
       belongs_to slideshow_relationship_name, class_name: "Refinery::Slideshow::Slideshow", autosave: true
 
+      # We create a method to define if this object needs an slideshow. By default it needs it.
+      define_method "needs_#{slideshow_relationship_name}?" do
+        true
+      end
+
+      # In slideshow accesor we check if this record has no slideshow but should have it, then we create a new slideshow
+      define_method "#{slideshow_relationship_name}" do
+
+        # We try to get the slideshow using the relationship
+        local_slideshow = self[slideshow_relationship_name.to_sym]
+
+        # If slideshow is nil, and the object has been already saved and the object needs an slideshow then we create it
+        if local_slideshow.nil? && !self.new_record? && self.send("needs_#{slideshow_relationship_name}?")
+          local_slideshow = self.send("build_#{slideshow_relationship_name}", attached: self)
+          self[slideshow_relationship_name.to_sym] = local_slideshow
+          send "#{slideshow_relationship_name}=", local_slideshow
+          self.save
+        end
+
+        local_slideshow
+
+      end
+
       # In slideshow assignation we should attach current object to the slideshow
       define_method "#{slideshow_relationship_name}=" do |slideshow|
         slideshow.attached = self
@@ -22,13 +45,10 @@ module Refinery::Slideshow::Concerns::ModelWithSlideShow
       end
 
       # Before saving we must check if this new slideshow has been created and create it otherwise
-      before_save do |record|
-        if record.send(slideshow_relationship_name).nil?
-          record.send "build_#{slideshow_relationship_name}", attached: record
-        else
-          record.send(slideshow_relationship_name).attached = record
+      after_save do |record|
+        if self.send("needs_#{slideshow_relationship_name}?")
+          record.send(slideshow_relationship_name).nil?
         end
-        record.send(slideshow_relationship_name).save!
       end
     end
 
--- spec/models/refinery/slideshow/slideshow_spec.rb
+++ spec/models/refinery/slideshow/slideshow_spec.rb
@@ -16,19 +16,47 @@ module Refinery
         let(:example_slideshow) { attached_page.example_slideshow }
         let(:not_attached_slideshow) { create :slideshow }
 
+        let( :needed_slideshow) { true }
+
         before {
 
+          # We attach the slideshow
           Refinery::Page.class_eval do
             include Refinery::Slideshow::Concerns::ModelWithSlideShow
             attach_slideshow :example_slideshow
           end
 
+          # If the slideshow is not needed then we modify page class
+          unless needed_slideshow
+            Refinery::Page.class_eval do
+              def needs_example_slideshow?
+                false
+              end
+            end
+          end
+
           attached_slideshow && not_attached_slideshow
+
         }
 
         it("should filter not attached ones") { Slideshow.not_attached.should match_array [not_attached_slideshow] }
-        it("should assign the slideshow to the attached object") { attached_page.slideshows.should match_array [attached_slideshow, example_slideshow] }
-        it("should create the example slideshow ") { attached_page.example_slideshow.should_not be_nil }
+
+        context "when slideshow is needed" do
+
+          it("should assign the slideshow to the attached object") { attached_page.slideshows.should match_array [attached_slideshow, example_slideshow] }
+          it("should create the example slideshow ") { attached_page.example_slideshow.should_not be_nil }
+
+        end
+
+        context "when slideshow is not needed" do
+
+          let( :needed_slideshow) { false }
+
+
+          it("should not assign the unnecessary slideshow") { attached_page.slideshows.should match_array [attached_slideshow] }
+          it("should not create the unnecessary example slideshow ") { attached_page.example_slideshow.should be_nil }
+
+        end
 
       end
 
